38a91ecf837af32e901729ffa963ab324f57e78b
Pedro Ávila <pedro2003dressler@gmail.com>
2022-04-01 15:51:20 -0300

    tentativa de incrementar a api de clipboard

---
+++ js/script.js
@@ -0,0 +1,10 @@
+navigator.clipboard;
+
+function copyToClipboard() {
+    var copyText = document.getElementById("content").value;
+    navigator.clipboard.writeText(copyText).then(() => {
+        // Alert the user that the action took place.
+        // Nobody likes hidden stuff being done under the hood!
+        alert("Copied to clipboard");
+    });
+  }
