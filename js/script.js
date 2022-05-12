490622d062acc428c0f161cd613942eeb3b7f3b6
Pedro Ávila <pedro2003dressler@gmail.com>
2022-05-12 17:26:30 -0300

    botão de email possui um popup para manualmente copiar o email ou clicando no icone de copiar

--- js/script.js
+++ js/script.js
@@ -1,16 +1,31 @@
-let gmail = document.getElementById('gmail').addEventListener('click', () => {
-    navigator.clipboard.writeText('pedro2003dressler@gmail.com');
-    alert('Email copiado!');
+const gmail = document.getElementById('gmail').addEventListener('click', () => {
+    // alert('Email copiado!');
+
+    var addImg = document.querySelector('.imgCopy');
+    addImg.removeAttribute('hidden');
+    var addSpan = document.querySelector('.spanEmail');
+    addSpan.removeAttribute('hidden');
+    var imgCopy = document.querySelector('#copiarEmail');
+    imgCopy.addEventListener('click', () => {
+        navigator.clipboard.writeText('pedro2003dressler@gmail.com');
+    });
 });
 
-let zap = document.getElementById('whats').addEventListener('click', () => {
+
+
+
+const zap = document.getElementById('whats').addEventListener('click', () => {
     window.open('https://wa.me/5531994903183');
 });
 
-let linkedin = document.getElementById('linkedin').addEventListener('click', () => {
+const linkedin = document.getElementById('linkedin').addEventListener('click', () => {
     window.open('https://www.linkedin.com/in/pedro-avila-dressler/');
 });
 
-let github = document.getElementById('github').addEventListener('click', () => {
+const github = document.getElementById('github').addEventListener('click', () => {
     window.open('https://github.com/PedroDressler');
 });
+
+ScrollOut({
+    targets: '.box'
+})
