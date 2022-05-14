6a4b4fcff3cbd5eabaad0b8079582997479425b5
Pedro Ávila <pedro2003dressler@gmail.com>
2022-05-14 11:30:57 -0300

    me preparando para o primeiro release do site pessoal

--- js/script.js
+++ js/script.js
@@ -1,31 +1,45 @@
-const gmail = document.getElementById('gmail').addEventListener('click', () => {
-    // alert('Email copiado!');
+window.addEventListener('load', (e)=>{
+  e.preventDefault();
+})
 
-    var addImg = document.querySelector('.imgCopy');
-    addImg.removeAttribute('hidden');
-    var addSpan = document.querySelector('.spanEmail');
-    addSpan.removeAttribute('hidden');
-    var imgCopy = document.querySelector('#copiarEmail');
-    imgCopy.addEventListener('click', () => {
-        navigator.clipboard.writeText('pedro2003dressler@gmail.com');
-    });
+const addImg = document.querySelector('.imgCopy');
+const addSpan = document.querySelector('.spanEmail');
+const imgCopy = document.getElementById('copiarEmail');
+const gmail = document.getElementById('gmail');
+
+gmail.addEventListener('click', () => {
+  addImg.removeAttribute('hidden');
+  addSpan.removeAttribute('hidden');
+  gmail.setAttribute('disabled', 'disabled')
 });
 
 
+imgCopy.addEventListener('click', () => {
+  const email = document.getElementById('email');
+  navigator.clipboard.writeText('pedro2003dressler@gmail.com');
+  email.textContent = 'Copiado!';
+  setTimeout(() => {
+    email.textContent = 'pedro2003dressler@gmail.com';
+  }, 1500);
+  imgCopy.classList.add('active');
+  setTimeout(() => {
+    imgCopy.classList.remove('active');
+  }, 200);
+});
 
 
 const zap = document.getElementById('whats').addEventListener('click', () => {
-    window.open('https://wa.me/5531994903183');
+  window.open('https://wa.me/5531994903183');
 });
 
 const linkedin = document.getElementById('linkedin').addEventListener('click', () => {
-    window.open('https://www.linkedin.com/in/pedro-avila-dressler/');
+  window.open('https://www.linkedin.com/in/pedro-avila-dressler/');
 });
 
 const github = document.getElementById('github').addEventListener('click', () => {
-    window.open('https://github.com/PedroDressler');
+  window.open('https://github.com/PedroDressler');
 });
 
 ScrollOut({
-    targets: '.box'
-})
+  targets: '.box'
+});
